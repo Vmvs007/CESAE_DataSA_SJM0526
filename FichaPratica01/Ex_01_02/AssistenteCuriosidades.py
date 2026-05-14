614b3724e82ba552ac06683df54f8c80370315d0
Vitor Santos <vmvs007@gmail.com>
2026-05-14 12:28:55 +0100

    FP01

---
+++ FichaPratica01/Ex_01_02/AssistenteCuriosidades.py
@@ -0,0 +1,48 @@
+from BibliotecaCuriosidades import *
+
+
+while True:
+    print("\n\n***** Assistente de Curiosidades *****")
+    print("1. Maior de Idade")
+    print("2. Fase da Vida")
+    print("3. Capital Pais")
+    print("4. Estação do Ano")
+    print("5. Dia da Semana")
+    print("6. Tipos de Números")
+
+    print("0. Sair")
+
+    opcao = int(input("Opção: "))
+
+    if opcao==1 :
+        print("\n** Maior de Idade **")
+    
+    elif opcao==2 :
+        print("\n** Fase da Vida **")
+    
+    elif opcao==3 :
+        print("\n** Capital País **")
+
+        paisInput = input("País: ")
+
+        capital = capital_pais(paisInput)
+
+        print(capital)
+
+        # print(capital_pais(paisInput))
+    
+    elif opcao==4 :
+        print("\n** Estação do Ano **")
+
+    elif opcao==5 :
+        print("\n** Dia da Semana **")
+
+    elif opcao==6 :
+        print("\n** Tipos de Números **")
+    
+    elif opcao==0 :
+        print("\n** Sair **")
+        break
+
+    else:
+        print(f"\n** Opção não reconhecida: {opcao} **")
